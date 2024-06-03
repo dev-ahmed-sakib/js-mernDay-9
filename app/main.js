3e20cdcc784e3b2f07a11027b671a4745c1eb38d
dev-ahmed-sakib <perfectsakiib.gmail.com>
2024-06-03 08:39:08 +0600

    of the end

--- app/main.js
+++ app/main.js
@@ -1,26 +1,82 @@
-// setAttribute (set some attibute list)
-const elements = document.getElementsByTagName('td');
-const storeData = [];
-let currentIndex = 0;
+//make some tags and acess they're previous and next tag using sibling element
+const taken = document.getElementsByTagName('p').nextElementSibling;
+console.log(taken)
 
-// Define a function to add and remove classes
-function toggleClass() {
-    // Remove class from previous element
-    if (currentIndex > 0) {
-        elements[currentIndex - 1].classList.remove('myNumber');
-    } else if (currentIndex === 0 && elements.length > 0) {
-        // Remove class from the last element when currentIndex resets to 0
-        elements[elements.length - 1].classList.remove('myNumber');
-    }
 
-    // Add class to the current element
-    elements[currentIndex].classList.add('myNumber');
 
-    // Move to the next element or reset to the beginning
-    currentIndex = (currentIndex + 1) % elements.length; 
-}
+//create a div  they has 2 children div and access they're firstElementChild & lastElementChild
+// const taken = document.getElementById('mainBox');
+// let getValue = taken.lastElementChild;
+// console.log(getValue)
 
-// Call the toggleClass function at regular intervals (every 1000ms or 1 second in this example)
+//access div classes  like css way
+// const taken = document.getElementById([id="mainBox"]);
+// console.log(taken.style.padding = '100px')
+
+//create a multiple div class and access these inside value children and color the div && parents div/class access
+// const taken = document.getElementsByTagName('td');
+// // const taken2 = document.getElementById('mainBox')
+// let getValue = taken.parentNode;
+// console.log(getValue)
+
+// const taken = document.querySelector('#mainBox');
+// let getValue = taken.children[0].children[0].children;
+// console.log(getValue)
+
+
+// //classList.contains
+// const taken2 = document.getElementById('mainBox').classList.add('myClass1')
+// const taken = document.getElementById('mainBox').classList.contains('myClass1');
+// console.log(taken)
+
+
+
+// //classList.remove
+// const taken2 = document.getElementById('mainBox').classList.remove('myClass1','myClass2','myClass3','myClass')
+// console.log(taken2)
+
+// //classList.add
+// const taken = document.getElementById('mainBox').classList.add('myClass1','myClass2','myClass3')
+// console.log(taken)
+
+
+// // removeAttribute
+// const taken = document.getElementById('mainBox').removeAttribute('class');
+// console.log(taken)
+
+
+// //hasAttribute
+// const taken = document.getElementById('mainBox').hasAttribute('class');
+// console.log(taken)
+
+//getAttribute
+// const taken = document.querySelector('.box').getAttribute('style');
+// console.log(taken)
+
+
+// // setAttribute (set some attibute list)
+// const elements = document.getElementsByTagName('td');
+// const storeData = [];
+// let currentIndex = 0;
+
+// // Define a function to add and remove classes
+// function toggleClass() {
+//     // Remove class from previous element
+//     if (currentIndex > 0) {
+//         elements[currentIndex - 1].classList.remove('myNumber');
+//     } else if (currentIndex === 0 && elements.length > 0) {
+//         // Remove class from the last element when currentIndex resets to 0
+//         elements[elements.length - 1].classList.remove('myNumber');
+//     }
+
+//     // Add class to the current element
+//     elements[currentIndex].classList.add('myNumber');
+
+//     // Move to the next element or reset to the beginning
+//     currentIndex = (currentIndex + 1) % elements.length; 
+// }
+
+// // Call the toggleClass function at regular intervals (every 1000ms or 1 second in this example)
 // setInterval(toggleClass, 1000);
 
 
