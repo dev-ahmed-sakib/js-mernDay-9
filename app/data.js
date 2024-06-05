86be9e5827f688c5285dc2799bb4148e6c29ec60
dev-ahmed-sakib <perfectsakiib.gmail.com>
2024-06-05 07:31:31 +0600

    facebook timelineData

---
+++ app/data.js
@@ -0,0 +1,37 @@
+const dev = [
+    {
+        authorInfo : {
+            name: "Muhammad Sakibul Islam",
+            profile: "https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-1/361943361_825238125785234_8812670503348895188_n.jpg?stp=dst-jpg_p200x200&_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFGW9r1O7Yi3gKZXKy_toBeGR7P6-AjQMQZHs_r4CNAxIxPepCiQbinu-uLG-U8DrItOsuIBDqzdyXWR3qMYHT-&_nc_ohc=iydpIz63VUoQ7kNvgFdXPfM&_nc_ht=scontent.fdac144-1.fna&oh=00_AYB2O9CAQlZIEAWAD3l3Z4fqoN0YjWrbgh_nKex5Pv8X9A&oe=66645A4F",
+            timeAgo: "5 min",
+        },
+        contents:{
+            P_caption: `Sea beach🏖️
+
+            P-Credit : Mohiuddin Murad (ভাইয়া)`,
+            P_content: "https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/415504243_909082014067511_6561888623328572148_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFxRs6846P-hgzhU4ukh3wjS846Skmy5-BLzjpKSbLn4MOsjfbnqBGPOh9JacuJ22-UncOQUM2EHIjVUb6NWLv0&_nc_ohc=D8B9YL2FggQQ7kNvgGx_R0s&_nc_ht=scontent.fdac144-1.fna&oh=00_AYC1dmeezAuN3_PqA1dsvOMq0FjI8Sa2ieOfi_zTKqtCGw&oe=6664381E",
+        },
+        reaction:{
+            like:"104",
+            comment:"24",
+        }
+    },
+    {
+        authorInfo : {
+            name: "Islam Zone",
+            profile: "https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-1/310354587_5884529188246632_3121440973554175026_n.jpg?stp=dst-jpg_p100x100&_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=qBHMLGN_1pkQ7kNvgHVbUHQ&_nc_ht=scontent.fdac144-1.fna&oh=00_AYD40baeoYsfwDoIbYHHyZoayqLa6eTzKV55Mb2OmUn7ig&oe=66656D1A",
+            timeAgo: "8 hours ago",
+        },
+        contents:{
+            P_caption: `সম্পদের রহস্য | The Secrets to Success | Episode 2
+            সম্পদের রহস্য | The Secrets to Success | Episode 2`,
+            P_content: "https://www.facebook.com/islamzone.org/videos/1119561025967740",
+        },
+        reaction:{
+            like:"568",
+            comment:"12",
+        }
+    },
+    
+
+]
